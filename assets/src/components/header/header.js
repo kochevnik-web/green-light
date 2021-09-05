7337bcdabebbb7d955260f64ba395aded96dcd3d
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-09-05 11:17:07 +0300

    Created about-2 section desctop

--- assets/src/components/header/header.js
+++ assets/src/components/header/header.js
@@ -2,7 +2,10 @@ import gsap from 'gsap';
 
 window.onload = function() {
     const hamburger = document.querySelector('.hamburger');
+    const duration = 1;
+
     hamburger.addEventListener('click', (e) => {
+
         hamburger.classList.toggle('is-active');
         document.querySelector('body').classList.toggle('show-menu');
         document.querySelector('.header-right').classList.toggle('menu-active');
@@ -18,4 +21,51 @@ window.onload = function() {
             });
         }
     });
+
+    if(window.innerWidth >= 768) {
+        gsap.from('#about-1-2', {
+            duration: duration,
+            y: '30em',
+            ease: "power3.out",
+            scrollTrigger: {
+                trigger: '#about-1',
+                start: 'top 35%',
+                end: 'bottom 60%',
+            }
+        });
+        gsap.from('#about-1-1', {
+            duration: duration,
+            rotate: 0,
+            ease: "power3.out",
+            scrollTrigger: {
+                trigger: '#about-1',
+                start: 'top 35%',
+                end: 'bottom 60%',
+            }
+        });
+        gsap.from('#about-1 h2', {
+            duration: duration,
+            opacity: 0,
+            scale: 2,
+            x: '40em',
+            y: '10em',
+            ease: "power3.out",
+            scrollTrigger: {
+                trigger: '#about-1',
+                start: 'top 35%',
+                end: 'bottom 60%',
+            }
+        });
+        gsap.from('#about-1 .about-subtitle', {
+            duration: duration,
+            y: '10em',
+            ease: "power3.out",
+            scrollTrigger: {
+                trigger: '#about-1',
+                start: 'top 35%',
+                end: 'bottom 60%',
+                // markers: true
+            }
+        });
+    }
 }
