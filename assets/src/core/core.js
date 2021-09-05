eafc92ab9dfeb0e8b17c319b501dec416c5674f7
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-09-05 20:14:52 +0300

    Added fancybox

--- assets/src/core/core.js
+++ assets/src/core/core.js
@@ -1,4 +1,4 @@
 // import '../components/owlcarousel/owl.carousel.js';
-// import '../components/fancybox/fancybox.js';
+import '../components/fancybox/fancybox.js';
 import '../components/main/main.js';
 import '../components/header/header.js';
