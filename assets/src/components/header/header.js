64f00eece8ed51ec3eed607117c7078ddd50a652
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-09-07 18:12:45 +0300

    added observe

--- assets/src/components/header/header.js
+++ assets/src/components/header/header.js
@@ -4,6 +4,35 @@ window.onload = function() {
     const hamburger = document.querySelector('.hamburger');
     const duration = 1;
 
+    const header = document.querySelector('header');
+
+    function goOnserver(entries) {
+        let isLeaving = false;
+        return new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    if(entry.target.hasAttribute('data-color-menu')){
+                        const attr = entry.target.getAttribute('data-color-menu');
+                        header.classList.add(attr);
+                    }
+                    isLeaving = true;
+                  } else if (isLeaving) {
+                    if(entry.target.hasAttribute('data-color-menu')){
+                        const attr = entry.target.getAttribute('data-color-menu');
+                        header.classList.add(attr);
+                        header.classList.remove(attr);
+                    }
+                    isLeaving = false;
+                }
+            });
+        }, {
+          rootMargin: '0px 0px -' + (window.innerHeight - 70) + 'px',
+        });
+    }
+
+    Array.from(document.getElementsByTagName('section')).forEach(section => goOnserver(section).observe(section))
+
+
     hamburger.addEventListener('click', (e) => {
 
         hamburger.classList.toggle('is-active');
